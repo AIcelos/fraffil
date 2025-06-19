e5b54f36ed240c2d48ccd8e682eb0a7aa362afd3
Sven <sdevries@casema.nl>
2025-06-19 02:33:59 +0200

    Fix all admin API exports: convert remaining ES6 exports to CommonJS

--- pages/api/admin/stats.js
+++ pages/api/admin/stats.js
@@ -1,7 +1,7 @@
 const googleSheetsService = require('../../../lib/googleSheets');
 const { getAllInfluencers } = require('../../../lib/database.js');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
